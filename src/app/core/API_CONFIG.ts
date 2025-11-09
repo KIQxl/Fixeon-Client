ccbd9d7c161234b6ae5b8c73ec229c75f1d76f3e
KIQxl <kaique_rba@yahoo.com.br>
2025-11-09 16:11:18 -0300

    [FEAT] nova URL para dominio fixeon.

--- src/app/core/API_CONFIG.ts
+++ src/app/core/API_CONFIG.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  BASE_URL: 'https://fixeon.onrender.com',
+  BASE_URL: 'https://api.fixeon.com.br',
   // BASE_URL: 'https://localhost:7016',
   AUTH: 'auth',
   TICKETS: 'tickets',
